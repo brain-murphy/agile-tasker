f391a94fca754cbe742c02f4c219ccd38c54aabd
Brian Murphy <brainmurphy1@gmail.com>
2015-10-10 19:36:46 -0400

    added reordering of list and refactored a lot of stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,6 @@
-var createTask = function(name, dueDate, workRem, importance, description) {
+var createTask;
+
+ createTask = function (name, dueDate, workRem, importance, description) {
     return {
         id:nextTaskId++,
         name:name ? name : '',
@@ -7,12 +9,12 @@ var createTask = function(name, dueDate, workRem, importance, description) {
         importance:importance ? importance : -1,
         desc:description ? description : ''
     };
-}
+};
 
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $rootScope, $ionicModal, $timeout) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -21,10 +23,10 @@ angular.module('starter.controllers', [])
     //$scope.$on('$ionicView.enter', function(e) {
     //});
 
-    $scope.taskList = [];
-    $scope.taskList.push(createTask("task1", "oct11", 15, 50, "sample_task"));
-    $scope.taskList.push(createTask("task2", "oct11", 10, 45, "samplasdfasdfasdf"));
-    $scope.taskList.push(createTask("task3", "oct11", 5, 40, "sample_tasasdffdasfsadfasdfk"));
+    $rootScope.taskList = [];
+    $rootScope.taskList.push(createTask("task1", "oct11", 15, 50, "sample_task"));
+    $rootScope.taskList.push(createTask("task2", "oct11", 10, 45, "samplasdfasdfasdf"));
+    $rootScope.taskList.push(createTask("task3", "oct11", 5, 40, "sample_tasasdffdasfsadfasdfk"));
 
 
     // Form data for the login modal
@@ -47,25 +49,43 @@ angular.module('starter.controllers', [])
 
     // Open the login modal
     $scope.addTask = function() {
+        console.log('taskData in addTask: ', $scope.taskData);
         $scope.taskAddModal.show();
     };
 
     // Perform the login action when the user submits the login form
     $scope.doTaskAdd = function() {
+        var isNewItem = true;
         console.log('adding task', $scope.taskData);
 
         //add task data to task list//
-        $scope.taskList.push($scope.taskData);
+        $rootScope.forEach(function (ele, i) {
+            if (ele.id === taskData.id) {
+                taskList.splice(i, 1, $scope.taskData);
+                isNewItem = false;
+            }
+        });
+
+        if (isNewItem) {
+            $rootScope.push($scope.taskData);
+        }
 
         //reset data in add task form//
         $scope.taskData = createTask();
 
         $scope.closeTaskAddModal();
     };
+
+    $scope.reorderTaskList = function () {
+
+    };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-
+.controller('TaskListCtrl', function($scope, $rootScope) {
+    $scope.moveItem = function(item, fromIndex, toIndex) {
+        $rootScope.taskList.splice(fromIndex, 1);
+        $rootScope.taskList.splice(toIndex, 0, item);
+    };
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
