654e3697121f3853d7f1fe1a66ea020f9b91e571
Jeffrey <jeffrey@unc.edu>
2015-10-10 18:52:52 -0400

    fixed scope issues

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,23 +5,18 @@ var createTask = function(name, dueDate, workRem, importance, description) {
         due_date:dueDate ? dueDate : -1,
         work_rem:workRem ? workRem : -1,
         importance:importance ? importance : -1,
-        desc:description ? description : ''
-        ranking = getRanking(due_date, work_rem, importance)
+        desc:description ? description : '',
+        ranking:getRanking(dueDate, workRem, importance)
     };
 }
 
-var updateListOrder(){
-    for (var i = 0; i < taskList.length; i++){
-        taskList.sort(function(a, b){return b.ranking - a.ranking});
-    }
-};
 
 var getRanking = function(due_date, work_rem, importance){
     //work remainging alsso needs to be in milliseconds
     var current_date = (new Date()).getTime();
     var ranking;
     if ((work_rem / (current_date - due_date)) >= 0.25){ //if the work remaining is 25% or more of the time left to complete task
-        return ((current_date - due_date) / work_rem) * 100);
+        return ((current_date - due_date) / work_rem) * 100;
     }
     else {
         return ((importance * work_rem) / (due_date - work_rem));
@@ -33,6 +28,12 @@ var getRanking = function(due_date, work_rem, importance){
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+    $scope.updateListOrder = function(){
+        for (var i = 0; i < $scope.taskList.length; i++){
+            $scope.taskList.sort(function(a, b){return b.ranking - a.ranking});
+        }
+    };
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -46,7 +47,7 @@ angular.module('starter.controllers', [])
     $scope.taskList.push(createTask("task2", "oct11", 10, 45, "samplasdfasdfasdf"));
     $scope.taskList.push(createTask("task3", "oct11", 5, 40, "sample_tasasdffdasfsadfasdfk"));
 
-    updateListOrder();
+    $scope.updateListOrder();
 
 
     // Form data for the login modal
