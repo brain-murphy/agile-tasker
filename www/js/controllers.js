55e27f9f9250de7fa0ec147148a481464454fb48
Jeffrey <jeffrey@unc.edu>
2015-10-10 20:26:04 -0400

    added timepicker button still not working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -37,6 +37,29 @@ angular.module('starter.controllers', [])
         }
     };
 
+    $scope.timePickerObject = {
+        inputEpochTime: ((new Date()).getHours() * 60 * 60),  //Optional
+        step: 15,  //Optional
+        format: 12,  //Optional
+        titleLabel: '12-hour Format',  //Optional
+        setLabel: 'Set',  //Optional
+        closeLabel: 'Close',  //Optional
+        setButtonType: 'button-positive',  //Optional
+        closeButtonType: 'button-stable',  //Optional
+        callback: function (val) {    //Mandatory
+            timePickerCallback(val);
+        }
+    };
+
+    function timePickerCallback(val) {
+        if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
+        } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), ':', selectedTime.getUTCMinutes(), 'in UTC');
+  }
+}
+
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
