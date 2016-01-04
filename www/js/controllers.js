c803fc20bffe2bd2af3d6dfbf11e5f46aacd892c
Brian Murphy <brianmurphy@gatech.edu>
2016-01-04 18:26:38 -0500

    new project initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+     
+.controller('prioritiesCtrl', function($scope) {
+
+})
+   
+.controller('viewTaskCtrl', function($scope) {
+
+})
+   
+.controller('editTaskCtrl', function($scope) {
+
+})
+   
+.controller('addATaskCtrl', function($scope) {
+
+})
+ 
