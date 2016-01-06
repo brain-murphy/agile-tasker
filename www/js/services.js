5cdea83045df67b543ccebb379b0078c29a39b7d
Brian Murphy <brianmurphy@gatech.edu>
2016-01-06 16:21:57 -0500

    changed task.name to task.title

--- www/js/services.js
+++ www/js/services.js
@@ -4,31 +4,31 @@ var servicesModule = angular.module('app.services', []);
 
 var MOCK_TASKS = {
 	'asdf': {
-		name: 'cs hw 5',
+		title: 'cs hw 5',
 		due:1000 * 3600 * 24 + Date.now(),
 		workRemaining: 1000 * 3600 * 3,
 		importance: 1
 	},
 	'asdf;lkj': {
-		name: 'register for classes',
+		title: 'register for classes',
 		due: 1000 * 3600 * 24 * 6 + Date.now(),
 		workRemaining:1000 * 3600 * 3,
 		importance: 2
 	},
 	'asdjklfsdjo': {
-		name: 'email Alyshia',
+		title: 'email Alyshia',
 		due: 1000 * 3600 * 24 + Date.now(),
 		workRemaining: 1000 * 60 * 20,
 		importance: 1
 	},
 	'asdfsddlk': {
-		name: 'plan meeting',
+		title: 'plan meeting',
 		due: 1000 * 3600 * 24 * 6 + Date.now(),
 		workRemaining: 1000 * 3600,
 		importance: 1.5
 	},
 	'kfdksla;lkd': {
-		name: 'document ticket',
+		title: 'document ticket',
 		due: 1000 * 3600 * 24 + Date.now(),
 		workRemaining: 1000 * 60 * 30,
 		importance: 1
